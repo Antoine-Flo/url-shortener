6037615e7b6421af3800c61dd7e9bc01a54f348f
Antoine-Flo <antoine.flouzat@gmail.com>
2020-12-10 16:19:39 +0100

    ejs layout finished

---
+++ public/app.js
@@ -0,0 +1,12 @@
+function copyUrl() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput");
+  
+    /* Select the text field */
+    copyText.select();
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+  }
+
