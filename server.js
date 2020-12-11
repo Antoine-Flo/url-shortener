0a3bfbac5d0cb31404150c2267c35cf58a688839
Antoine-Flo <antoine.flouzat@gmail.com>
2020-12-11 16:52:36 +0100

    make it better

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ app.get('/', (req, res) => {
   })
 })
 
-
-app.listen(3000, function () {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("Server started on port 3000");
 });
 
